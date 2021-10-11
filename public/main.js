ba75f18df3419c0f14b51fa9cd9a849105322535
sankaire <kerenketepela@gmail.com>
2021-10-11 20:14:08 +0300

    add vote count functionality

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", (e) =>{
     const choice = document.querySelector("input[name = zt]:checked").value
     const data = {zt:choice}
     
-    fetch("http://localhost:3000/poll", {
+    fetch("http://localhost:3002/poll", {
         method:"post",
         body:JSON.stringify(data),
         headers: new Headers({
@@ -18,45 +18,52 @@ form.addEventListener("submit", (e) =>{
 
     e.preventDefault()
 })
-
-//chart
-let dataPoints = [
-    {label : "Peter", y:0},
-    {label : "Tepela", y:0},
-    {label : "Suzan", y:0},
-    {label : "Kamau", y:0},
-]
-
-const chartContainer = document.querySelector("#chartContainer")
-if(chartContainer){
-    const chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled : true,
-        theme:"theme1",
-        title:{
-            text:"Zetech Ballots Results"
-        },
-        data:[{
-            type:"column",
-            dataPoints:dataPoints
-        }]
+fetch("http://localhost:3002/poll")
+    .then(res => res.json())
+    .then(data =>{
+        const votes = data.votes
+        const totalVotes = votes.length
+        //count vote points
+        const voteCounts = votes.reduce((acc, vote) =>(acc[vote.zt]=((acc[vote.zt]|| 0)+ parseInt(vote.points)), acc), {})
+            let dataPoints = [
+                {label : "Peter", y:voteCounts.Peter},
+                {label : "Tepela", y:voteCounts.Tepela},
+                {label : "Suzan", y:voteCounts.Suzan},
+                {label : "Kamau", y:voteCounts.Kamau},
+            ]
+            
+            const chartContainer = document.querySelector("#chartContainer")
+            if(chartContainer){
+                const chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled : true,
+                    theme:"theme1",
+                    title:{
+                        text:`Total Votes ${totalVotes}`,
+                    },
+                    data:[{
+                        type:"column",
+                        dataPoints:dataPoints
+                    }]
+                })
+                chart.render()
+                // Enable pusher logging - don't include this in production
+                Pusher.logToConsole = true;
+            
+                var pusher = new Pusher('5eebcaa203111be8fb46', {
+                  cluster: 'ap2',
+                  encryped:true
+                });  
+                var channel = pusher.subscribe('zt-poll');
+            
+                channel.bind('zt-vote', function(data) {
+                  dataPoints.forEach((point)=>{
+                      if(point.label==data.zt)
+                      {
+                           point.y+=data.points;
+                      }
+                  });
+                  chart.render();
+                })
+            }            
     })
-    chart.render()
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
 
-    var pusher = new Pusher('5eebcaa203111be8fb46', {
-      cluster: 'ap2',
-      encryped:true
-    });  
-    var channel = pusher.subscribe('zt-poll');
-
-    channel.bind('zt-vote', function(data) {
-      dataPoints.forEach((point)=>{
-          if(point.label==data.zt)
-          {
-               point.y+=data.points;
-          }
-      });
-      chart.render();
-    })
-}
