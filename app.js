d959da5454781141f304bb523e45a5b80b899bb1
serahshani <kerenketepela@gmail.com>
2021-12-07 16:33:44 +0300

    code formatting

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const path = require("path")
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 //DB config
-require("./config/db.js")
+require("./config/db.js");
 
 //app
-const app = express()
+const app = express();
 
 //routing
-const poll = require("./routes/poll")
+const poll = require("./routes/poll");
+// const president = require("./routes/poll/president");
 
 //set public directory
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 //body parser midlewere
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //enable cors
-app.use(cors())
+app.use(cors());
 
-app.use("/poll", poll)
+app.use("/poll", poll);
+// app.use("/president", president);
 
 //port number
-const port = 3002
+const port = 3002;
 
 //start server
-app.listen(port, () => console.log(`server started on ${port}`))
+app.listen(port, () => console.log(`server started on ${port}`));
