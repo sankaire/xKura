987c35510a6c777f87b984a99f45441ff1897138
serahshani <kerenketepela@gmail.com>
2021-12-12 22:58:40 +0300

    Chore:code formating

--- config/db.js
+++ config/db.js
@@ -2,19 +2,22 @@
 // const url = 'mongodb+srv://tepela:ZAS0706x@cluster0.ns3va.mongodb.net/cluster0?retryWrites=true&w=majority'
 
 // const dbName = "polls"
-// MongoClient.connect(url, { useNewUrlParser: true }, 
+// MongoClient.connect(url, { useNewUrlParser: true },
 //     (err, client) => {
 //     if (err) return console.log(err)
-  
+
 //     // Storing a reference to the database so you can use it later
 //     db = client.db(dbName)
 //     console.log(`Connected MongoDB: ${url}`)
 //     console.log(`Database: ${dbName}`)
 //   })
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb+srv://tepela:ZAS0706x@cluster0.ns3va.mongodb.net/cluster0?retryWrites=true&w=majority")
-.then(() => console.log("Mongo DB connected"))
-.catch(err => console.log(err))
+mongoose
+  .connect(
+    "mongodb+srv://tepela:ZAS0706x@cluster0.ns3va.mongodb.net/cluster0?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Mongo DB connected"))
+  .catch((err) => console.log(err));
