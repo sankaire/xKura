fa36ffba7d1de577e31a6087c856fc8cb2a3b77a
serahshani <kerenketepela@gmail.com>
2021-12-12 22:55:31 +0300

    Feat: authenticate users

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const expresslayouts = require("express-ejs-layouts");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+require("./config/passport")(passport);
+
 // const firebase = require("firebase");
 const firebase = require("firebase/app");
 require("firebase/analytics");
@@ -28,7 +35,9 @@ firebase.initializeApp(firebaseConfig);
 require("./config/db.js");
 
 //routing
+const user = require("./routes/users");
 const poll = require("./routes/poll");
+//
 // const president = require("./routes/poll/president");
 
 //set public directory
@@ -37,15 +46,39 @@ app.use(express.static(path.join(__dirname, "public")));
 //body parser midlewere
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //enable cors
 app.use(cors());
 
+app.use(expresslayouts);
+app.set("view engine", "ejs");
+
 app.use("/poll", poll);
-// app.use("/president", presidJent);
-
-//port number
-const port = 3002;
-
+app.use("/users", user);
+// app.use("/president", presidJent)
 //start server
+
+const port = 3002;
 app.listen(port, () => console.log(`server started on ${port}`));
