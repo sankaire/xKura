629eda4f5a9861dff29b1ea1de9d2e5823fa6ce6
serahshani <kerenketepela@gmail.com>
2021-12-12 22:57:35 +0300

    Fix:user model

---
+++ model/User.js
@@ -0,0 +1,24 @@
+const moongose = require("mongoose");
+
+const userSchema = new moongose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = moongose.model("User", userSchema);
+
+module.exports = User;
